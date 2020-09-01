66cf4780b98f4933e86cd7056ed0a68d9dbb66d3
mcgraths7 <mcgrath.steven7@gmail.com>
2020-09-01 09:43:45 -0400

    implemented a queue

---
+++ queue/queue.js
@@ -0,0 +1,36 @@
+class Queue {
+  constructor() {
+    this.data = [];
+    this.top = 0;
+  }
+  shift() {
+    if (!this.isEmpty()) {
+      const ele = this.data[0];
+      this.data.splice(0, 1);
+      this.top -= 1;
+      return ele;
+    }
+    throw new Error('Queue is empty');
+  }
+
+  unshift(ele) {
+    this.top += 1;
+    this.data.splice(0, 0, ele);
+    return this.data;
+  }
+
+  peek() {
+    if (!this.isEmpty()) {
+      return this.data[0];
+    }
+    throw new Error('Queue is empty');
+  }
+
+  length() {
+    return this.top;
+  }
+
+  isEmpty() {
+    return this.top === 0;
+  }
+}
